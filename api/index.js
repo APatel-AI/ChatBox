0fba6d1aa4a97cc4b234c319956ee2da3f176bcc
APatel-AI <arpan.p.se@gmail.com>
2024-05-05 18:47:18 -0500

    created the back-end register function

--- api/index.js
+++ api/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
-
+import 'dotenv/config'
+import mongoose from 'mongoose';
+import User from './models/User';
+var jwt = require('jsonwebtoken');
+mongoose.connect(process.env.MONGO_URL);
+console.log(process.env.MONGO_URL)
 const app = express();
 
+const jwtSecret = process.env.JWT_SECRET;
 app.get('/test', (req,res) => {
     res.json('test ok')
 
 })
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body; 
+    const createdUser = await User.create({username, password});
+    jwt.sign({userId:createdUser,_id}, jwtSecret, (err, token) => {
+        if (err) throw err;
+        res.cookie('token', token).status(201).json('ok');
+    });
+})
+
 
 app.listen(4000);
---
+++ api/models/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+
+    username: {type:String, unique:true}, 
+    password: String, 
+
+
+}, {timestamps: true});
+
+
+export const UserModel = mongoose.model('User', UserSchema);
