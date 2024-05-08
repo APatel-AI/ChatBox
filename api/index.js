56b49ffb0fc995e3f299b06859226f5dc78d4635
APatel-AI <arpan.p.se@gmail.com>
2024-05-08 17:44:53 -0500

    user registration backend working successfully

--- api/index.js
+++ api/index.js
@@ -23,16 +23,17 @@ app.get("/test", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-    const { username, password } = req.body;
-    try {
-      const createdUser = await User.create({ username, password });
-      const token = jwt.sign({ userId: createdUser._id }, jwtSecret);
-      res.cookie("token", token).status(201).json({ message: "Registration successful" });
-    } catch (err) {
-      console.error("Error during registration:", err);
-      res.status(500).json({ error: "Registration failed" });
-    }
+  const { username, password } = req.body;
+  try {
+    const createdUser = await User.create({ username, password });
+    const token = jwt.sign({ userId: createdUser._id }, jwtSecret);
+    res.cookie("token", token).status(201).json({ message: "Registration successful" });
+  } catch (err) {
+    console.error("Error during registration:", err);
+    res.status(500).json({ error: "Registration failed" });
+  }
 });
+
   
 
 
